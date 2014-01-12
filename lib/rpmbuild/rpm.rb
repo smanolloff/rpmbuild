dfb68c349c7c0b296e79b87d7dd9c04db646d89c
Simeon Manolov <s.manolloff@gmail.com>
2014-01-12 20:19:32 +0200

    remove unneeded comments

--- lib/rpmbuild/rpm.rb
+++ lib/rpmbuild/rpm.rb
@@ -78,44 +78,4 @@ class Rpmbuild::Rpm
     # The first group of the last match
     output.scan(pattern).last.first
   end
-
-
-
-
-  # def rpm_location(time = Time.now)
-  #   if self.new_record?
-  #     "#{RPMS_DIR}/noarch/circle_customization_#{common_name(time)}-#{full_release}.noarch.rpm"
-  #   else
-  #     "#{RPMS_DIR}/noarch/#{self.rpm}"
-  #   end
-  # end
-
-  # def common_name(time = @timestamp)
-  #   "#{country_abbr}_#{customer}-#{version(time)}"
-  # end
-
-  # def full_release
-  #   @timestamp ||= Time.now 
-  #   r = release
-  #   t = @timestamp.strftime("%H.%M")
-  #   el = centos_version.to_str[/\d+/]
-
-  #   return "#{t}_el#{el}_r#{r}"
-  # end
-
-
-  # def self.parse_template_release
-  #   spec_file = SpecFile.new
-  #   return spec_file.release
-
-  #   # template_regexp = /^Version: {::VERSION::}.+^Release: {::RELEASE::}_r(\d+)/m
-  #   # raise MissingSpecTemplateError, CUSTO_TEMPLATE unless File.file?(CUSTO_TEMPLATE)
-  #   # raise InvalidSpecTemplateError, CUSTO_TEMPLATE unless template_regexp.match(File.read(CUSTO_TEMPLATE))
-  #   # return $1.to_i
-  # end
-
-
-  # def version(time)
-  #   time.strftime("%Y.%m.%d")
-  # end
 end
